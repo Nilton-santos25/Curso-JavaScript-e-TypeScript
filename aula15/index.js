d54f1cd807a724e0f23137113401db3520a81d5e
Nilton Santos <niltonsantosmf2@gmail.com>
2022-02-26 18:09:43 -0300

    Math e Exercícios

---
+++ aula15/index.js
@@ -0,0 +1,10 @@
+let num1 = 9;
+
+// console.log(num1 ** 0.5); //pegando a raiz quadrada
+//let num2 = Math.floor(num1); //arredonda para baixo
+// let num2 = Math.ceil(num1); //arredonda para cima
+//let num2 = Math.round(num1); //arredonda para o número mais próximo
+// console.log(Math.max(1,2,3,4,5,-10,-50,1500,70,30,222)); //maior número da sequência
+// console.log(Math.min(1,2,3,4,5,-10,-50,1500,70,30,222)); //menor número da sequência
+// const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+// console.log(aleatorio);
