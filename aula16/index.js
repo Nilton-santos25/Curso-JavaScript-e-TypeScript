904aae1f267e52a946ff17db2b21cf59ec315e2d
Nilton Santos <niltonsantosmf2@gmail.com>
2022-02-28 15:46:52 -0300

    arrays

---
+++ aula16/index.js
@@ -0,0 +1,33 @@
+//ìndices          0       1           2       3
+const alunos = ['Maria', 'Antonio', 'João', 'Márcio'];
+
+alunos.push('Jurema');
+alunos.push('Maria Clara');
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array);
+
+// console.log(alunos.slice(0, 3)); //fatiando o array
+// console,log(alunos.slice(0, -2));
+
+//console.log(alunos[50]); 
+
+// delete alunos[1];
+// console.log(alunos);
+
+// const removido = alunos.pop();
+// console.log(removido);
+// alunos.pop(); //remove elementos do final do array
+// console.log(alunos);
+
+//alunos.pop(); //remove índice do fim
+//alunos.shift(); //remove o indice no começo
+
+// alunos[0] = 'Eduardo'; //editar o valor do array
+// alunos.unshift('Adolfo'); //adicionando no início
+
+// alunos.push('Amanda');//colocando elentos no final do array com push
+// alunos[4] = 'Luiza' //colocando elementos em um array
+// console.log(alunos);
+// console.log(alunos[1]);
+// console.log(alunos[2]);
