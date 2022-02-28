8522a04da718975d448ecddc3afb7e9291278a58
Nilton Santos <niltonsantosmf2@gmail.com>
2022-02-28 16:23:52 -0300

    funções

--- aula17_Funçoes/index.js
+++ aula17_Funçoes/index.js
@@ -1,4 +1,33 @@
-function saudacao(){
-    console.log('Olá, boa tarde!');
-}
-saudacao();
+//ARROW FUNCTION 
+const raiz = (num1) => num1 ** 0.5
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
+
+
+// const raiz = function(num) {
+//     return num ** 0.5
+// };
+
+// console.log(raiz(9));
+// console.log(raiz(18));
+// console.log(raiz(27));
+
+
+// function soma(n1, n2) {
+//     const resultado = n1 + n2;
+//     return resultado;
+// }
+
+// const resultado = soma();
+// console.log(resultado);
+
+// console.log(resultado); erro pois está acessando um valor que está dentro do escopo da função
+
+// function saudacao(nome) {
+//     return `Olá, boa tarde! ${nome}`
+// }
+
+// const variavel = saudacao('Maria');
+// console.log(variavel);
