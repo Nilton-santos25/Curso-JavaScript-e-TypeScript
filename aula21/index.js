ba83e492a3f3d00351d3cdfa42404b65279f9895
Nilton Santos <niltonsantosmf2@gmail.com>
2022-03-01 14:24:08 -0300

    operadores de comparação

---
+++ aula21/index.js
@@ -0,0 +1,13 @@
+/*
+Operadores de comparação
+> maior que 
+>= maior ou igual a
+< menor que
+<= menor ou igual a
+== igualdade 
+=== igualdade estrita (idêntico) verifica valor e tio
+!= diferente
+!== direferente estrito (verifica valor e tipo)
+*/
+const comp = 10 >= 5;
+console.log(comp);
