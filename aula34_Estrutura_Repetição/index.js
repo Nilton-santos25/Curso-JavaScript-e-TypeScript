3feb0242eee8a540f7600c34b124178c276a6b78
Nilton Santos <niltonsantosmf2@gmail.com>
2022-03-21 20:38:43 -0300

    estrutura for

---
+++ aula34_Estrutura_Repetição/index.js
@@ -0,0 +1,13 @@
+// i - endex
+for (let i = 0; i <= 10; i++) {
+    const par = i % 2 === 0 ? 'Par' : 'Impar'
+    console.log(i ,par);
+}
+
+
+//                 1      2        3      4            5
+const frutas = ['Maça', 'Pêra', 'Uva', 'Banana', 'Acerola'];
+
+for (let i = 0; i< frutas.length; i++) {
+    console.log(`ìndice ${i}`, frutas[i])
+}
