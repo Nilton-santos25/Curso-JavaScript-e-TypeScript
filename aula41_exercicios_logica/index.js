065f978ff30824e2be40eb6e2b78e03c58336b08
Nilton Santos <niltonsantosmf2@gmail.com>
2022-04-18 20:57:32 -0300

    Exercicios_Laços_Repetição

---
+++ aula41_exercicios_logica/index.js
@@ -0,0 +1,9 @@
+//Escreva uma funcao que receba 2 números e retorne o maior deles
+
+function maior(num1, num2) {
+    return num1 > num2 ? num1 : num2;
+}
+
+const maior2 = (x, y) => x > y ? x : y; 
+
+console.log(maior2(100,10))
---
+++ aula42_Ex/index.js
@@ -0,0 +1,7 @@
+//Escreva uma função chamada ePaisagem que
+//recebe dois argumentos, largura e altura
+//de uma Imagem e retorne true se a imagem estiver nodo paisagem
+
+const ePaisagem = (largura, altura) => largura > altura;
+
+console.log(ePaisagem(1080, 1080));
